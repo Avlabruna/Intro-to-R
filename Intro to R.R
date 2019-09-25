9cad17e1cdcb5e65fc80bec03e32f954e7300d48
Anthony LaBruna <avl323@lehigh.edu>
2019-09-25 14:41:43 -0400

    Deleted *.Data

--- Intro to R.R
+++ Intro to R.R
@@ -1,9 +1,9 @@
-# R script to accopany Intro to R
-# Copy for RStudent 2019
+# R script to accompany Intro to R
+# Anthony Vincent LaBruna
 ######################################################
 
-# You will see that you can put non-executable "comment" lines
-# in your script by putting "#" at the beginning of the lines.
+# You will see that you can put non-executable "comment" lines.
+# in your script by putting "#" at the beginning of the lines...
 # 
-# In fact, you can put comments anywhere on a line as long as 
+# In fact, you can put comments anywhere on a line as long as..
 # you put "#" before them.
